3ef44e2090848f36ba0dd572f92aa64606e1e65f
NandozXx <fernandojrbarbosa8@gmail.com>
2025-03-06 18:23:40 -0300

    Add files via upload

---
+++ Desafionovato.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+
+    char estadoA;
+    char estadoB;
+    char codigoA[10];
+    char codigoB[10];
+    char cidadeA[50];
+    char cidadeB[50];
+    float areaA, areaB, pibA, pibB;
+    int populacaoA, populacaoB, pontos_turisticosA, pontos_turisticosB;
+
+   
+    printf("\nCodigo da Carta: 01");
+    printf("\nEstado: Paraiba");
+    printf("\nCidade: Joao Pessoa");
+    printf("\nÁrea: 56.467.242 km² ");
+    printf("\nPib: 77.470 bilhões");
+    printf("\nPopulação: 3.975 milhões");
+    printf("\nPontos Turísticos: 30\n");
+   
+
+
+    printf("\nCodigo da Carta: 02");
+    printf("\nEstado: Amazonas");
+    printf("\nCidade: Manaus");
+    printf("\nÁrea: 1.571.000 km²");
+    printf("\nzPib: 42.968 milhões");
+    printf("\nPopulação: 3,942 milhões");
+    printf("\nPontos Turísticos: 20\n");
+    printf("\n");
+
+
+    return 0;
+
+
+
+
+
+}
+
