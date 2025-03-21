85ddd243767604cf82e2bc76b0b22fbb71c3731e
NandozXx <fernandojrbarbosa8@gmail.com>
2025-03-21 14:14:31 -0300

    Update batalhaNavalNovato.c

--- batalhaNavalNovato.c
+++ batalhaNavalNovato.c
@@ -14,6 +14,7 @@ int main() {
         matriz[5 + i][7] = navio;
 
     // Exibe o tabuleiro
+    printf(" *B A T A L H A  N A V A L*\n\n");
     printf("   A B C D E F G H I J\n");
     for (i = 0; i < 10; i++) {
         printf("%2d ", i);
@@ -24,3 +25,4 @@ int main() {
 
     return 0;
 }
+
